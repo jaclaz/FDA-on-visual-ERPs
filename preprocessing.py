b2f923e78211a676584e75ad52b77faf91fca481
P0cO0 <101203110+jaclaz@users.noreply.github.com>
2022-10-27 08:59:56 +0200

    Added files
    In epochs_dropped.xlsx are present the percentages of epochs rejected for every patient after the preprocessing phase

--- preprocessing.py
+++ preprocessing.py
@@ -22,10 +22,10 @@ def Preprocessing(paziente, home_path):
     
     categories = pd.read_csv(r"C:\Users\Asus\Downloads\things_concepts.tsv", sep='\t')
     
-    filename = home_path+"\sub-0"+str(paziente)+"\eeg\sub-0"+str(paziente)+"_task-rsvp_events.csv"
+    filename = home_path+"\sub-"+str(paziente)+"\eeg\sub-"+str(paziente)+"_task-rsvp_events.csv"
     df_events = pd.read_csv(filename)
     
-    filename = home_path+"\sub-0"+str(paziente)+"\eeg\sub-0"+str(paziente)+"_task-rsvp_eeg.vhdr"
+    filename = home_path+"\sub-"+str(paziente)+"\eeg\sub-"+str(paziente)+"_task-rsvp_eeg.vhdr"
     raw=mne.io.read_raw_brainvision(filename, preload=True)
     filtro=raw.copy().filter(0.1, 12, method='fir')
     reference=filtro.copy().set_eeg_reference(ref_channels='average')
@@ -42,15 +42,15 @@ def Preprocessing(paziente, home_path):
     ica.exclude = eog_indices
     ica.apply(reconst_raw)
     
-    annot=reconst_raw.annotations
+    #Rimuovo il canale di reference per evitare che venga conteggiato nelle epochs rimosse
+    final_raw=reconst_raw.drop_channels('Reference')
     
-    events, event_id = mne.events_from_annotations(reconst_raw)
-    #reject_criteria = dict(eeg=150e-6)    
-    #epochs = mne.Epochs(reconst_raw, events, tmin=-0.1, tmax=1, reject=reject_criteria)
+    events, event_id = mne.events_from_annotations(final_raw)
+    reject_criteria = dict(eeg=150e-6)    
 
-    epochs = mne.Epochs(reconst_raw, events, tmin=-0.1, tmax=1)
-    #epochs.drop_bad(reject=reject_criteria)
-    #epochs.plot_drop_log()
+    epochs = mne.Epochs(final_raw, events, tmin=-0.1, tmax=1)
+    epochs.drop_bad(reject=reject_criteria)
+    epochs.plot_drop_log()
     
     #Estraggo solo le epochs che registrano l'esperimento
     data=epochs['10001']
@@ -71,15 +71,15 @@ def Preprocessing(paziente, home_path):
     #imposto come codice evento i numeri associati ai concepts di THINGS
     #MEMO: devo costruire un nuovo dizionario per gli events
     
-    for ii in np.arange(24648):
+    for ii in np.arange(len(new_events[:])):
         new_events[ii][2]=prova[ii]
         
     #data.save('sub-'+str(paziente)+'_task-rsvp_epochs.fif', overwrite=True)
     
     return data, df_events, BU_categories, categories, ord_cat, new_events, obj_num, prova
 
-#for ii in range(41, 51):
-data, df_events, categories, tabellone, ord_cat, new_events, obj_num, prova = Preprocessing(4, home_path)
+for ii in range(41, 51):
+    data, df_events, categories, tabellone, ord_cat, new_events, obj_num, prova = Preprocessing(ii, home_path)
     
     
     
